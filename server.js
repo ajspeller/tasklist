03b6520abf236a8cc4cf1d30ef4d7e3a01111977
AJ Speller <ajspeller@gmail.com>
2018-06-24 04:14:17 -0400

    Setup server

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+
+var index = require('./routes/index');
+var tasks = require('./routes/tasks');
+
+var port = 3000;
+var app = express();
+
+// view engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
+
+// set static folder, where the angular front-end files will leave
+app.use(express.static(path.join(__dirname, 'client')));
+
+// body-parser middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
+app.use('/', index);
+app.use('/api', tasks);
+
+app.listen(port, () => {
+  console.log(`Server stared on port .. ${port}`);
+});
+
+
